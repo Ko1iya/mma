232252d05f42373180b8007219a7d17343d02674
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-07-09 13:11:51 +0800

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react"
+import styles from "./app.module.css"
+
 import MyApp from "./components/MyApp/MyApp"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  // console.log(styles)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <div className='App'>
-      <MyApp />
+    <div className='app'>
+      {loading ? (
+        <div className={styles.loading}>
+          <div className={styles.img}></div>
+          <div className={styles.loader}>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+            <span className={styles.dot}></span>
+          </div>
+        </div>
+      ) : (
+        <MyApp />
+      )}
     </div>
   )
 }
