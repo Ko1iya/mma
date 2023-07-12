76966839fc5d61178858b5ec6520ac27fd0427f5
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-07-12 13:29:03 +0800

    mouse listener in swiper ready

--- src/components/Swiper/Swiper.tsx
+++ src/components/Swiper/Swiper.tsx
@@ -2,16 +2,46 @@ import "swiper/css"
 
 import styles from "./swiper.module.css"
 import SwiperItem from "./SwiperItem"
+import { useStateRef, getRefValue } from "../../hooks"
+import { useRef } from "react"
 
 interface SwiperProps {
   items?: string[]
 }
 
 function Swiper({ items }: SwiperProps) {
+  const currentOffsetXRef = useRef(0)
+  const startXRef = useRef(0)
+  const [offsetX, setOffsetX, offsetXRef] = useStateRef(0)
+
+  const onMouseMove = (e: MouseEvent) => {
+    const currentX = e.clientX
+    const diff = getRefValue(startXRef) - currentX
+    const newOffsetX = getRefValue(currentOffsetXRef) - diff
+
+    setOffsetX(newOffsetX)
+  }
+
+  const onMouseUp = () => {
+    window.removeEventListener("mouseup", onMouseUp)
+    window.removeEventListener("mousemove", onMouseMove)
+  }
+
+  const onMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    currentOffsetXRef.current = getRefValue(offsetXRef)
+    startXRef.current = e.clientX
+
+    window.addEventListener("mousemove", onMouseMove)
+    window.addEventListener("mouseup", onMouseUp)
+  }
+
   return (
     <div className={styles.swiper}>
-      <div className={styles.swiperContainer}>
-        <ul className={styles.swiperList}>
+      <div className={styles.swiperContainer} onMouseDown={onMouseDown}>
+        <ul
+          className={styles.swiperList}
+          style={{ transform: `translate3d(${offsetX}%, 0, 0)` }}
+        >
           {items &&
             items.map((item, idx) => <SwiperItem key={idx}>{item}</SwiperItem>)}
         </ul>
